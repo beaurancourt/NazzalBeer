381859a52a54a4ea7715094f6332cf3dfed16fa5
Beau Shinkle <beaushinkle@gmail.com>
2014-04-17 00:18:52 -0400

    removed unnecessary comments from beer2

--- beer2.py
+++ beer2.py
@@ -10,10 +10,12 @@ model.n = Param(default=n)
 model.max = Param(default=600)
 model.M = Param(default=600)
 model.index = RangeSet(model.n)
-paleAleDemand = [0, 200, 260, 300, 270, 270, 280, 320, 350, 420, 600, 700, 400, 400, 400, 420, 450, 480, 410, 350, 250, 200, 300, 300, 330, 420, 450]
-stoutDemand = [0, 50, 70, 80, 100, 120, 150, 150, 180, 180, 150, 150, 150, 150, 180, 240, 250, 250, 200, 200, 200, 200, 190, 200, 160, 150, 190]
-# summerAleDemand = [0]*27
-winterBrewDemand = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 500, 200, 200, 200, 600, 200, 200, 200, 200, 300, 200, 200, 160, 120, 120, 0]
+paleAleDemand = [0, 200, 260, 300, 270, 270, 280, 320, 350, 420, 600, 700, 400, 400, 400, 420, 450, 480, 410, 350, 250, \
+  200, 300, 300, 330, 420, 450]
+stoutDemand = [0, 50, 70, 80, 100, 120, 150, 150, 180, 180, 150, 150, 150, 150, 180, 240, 250, 250, 200, 200, 200, 200, \
+  190, 200, 160, 150, 190]
+winterBrewDemand = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 500, 200, 200, 200, 600, 200, 200, 200, 200, 300, 200, 200, 160, \
+  120, 120, 0]
 octoberfestDemand = [0, 0, 250, 250, 350, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 model.paleAle1 = Var(model.index, bounds=(0, model.max))
@@ -80,21 +82,6 @@ model.stoutBin2Constraint = Constraint(model.index, rule=stoutBin2)
 def stoutBin3(model, i):
   return 600 * model.yStout3[i] >= model.stout3[i]
 model.stoutBin3Constraint = Constraint(model.index, rule=stoutBin3)
-
-# model.summerAle1 = Var(model.index, within=Integers, bounds=(0, model.max))
-# model.summerAle2 = Var(model.index, within=Integers, bounds=(0, model.max))
-# model.summerAle3 = Var(model.index, within=Integers, bounds=(0, model.max))
-
-# model.ySummerAle1 = Var(model.index, domain=Binary)
-# model.ySummerAle2 = Var(model.index, domain=Binary)
-# model.ySummerAle3 = Var(model.index, domain=Binary)
-
-# def summerAleBin1(model, i):
-#   return 600 * model.ySummerAle1[i] >= model.summerAle1[i]
-# def summerAleBin2(model, i):
-#   return 600 * model.ySummerAle2[i] >= model.summerAle2[i]
-# def summerAleBin3(model, i):
-#   return 600 * model.ySummerAle3[i] >= model.summerAle3[i]
 
 model.winterBrew1 = Var(model.index, bounds=(0, model.max))
 model.winterBrew2 = Var(model.index, bounds=(0, model.max))
@@ -180,16 +167,6 @@ model.sellStout0 = Var(model.index, within=NonNegativeReals)
 model.sellStout1 = Var(model.index, within=NonNegativeReals)
 model.sellStout2 = Var(model.index, within=NonNegativeReals)
 model.sellStout3 = Var(model.index, within=NonNegativeReals)
-
-# model.inventorySummerAle0 = Var(model.index)
-# model.inventorySummerAle1 = Var(model.index)
-# model.inventorySummerAle2 = Var(model.index)
-# model.inventorySummerAle3 = Var(model.index)
-
-# model.sellSummerAle0 = Var(model.index)
-# model.sellSummerAle1 = Var(model.index)
-# model.sellSummerAle2 = Var(model.index)
-# model.sellSummerAle3 = Var(model.index)
 
 model.inventoryWinterBrew0 = Var(model.index)
 model.inventoryWinterBrew1 = Var(model.index)
@@ -328,58 +305,6 @@ def sellStout0Demand(model, i):
   return model.sellStout0[i] <= stoutDemand[i] - model.sellStout3[i] - model.sellStout2[i] - model.sellStout1[i]
 model.sellStout0DemandConstraint = Constraint(model.index, rule=sellStout0Demand)
 
-### SummerAle Inventory
-
-# def balanceSummerAle0(model, i):
-#   if i == 1:
-#     return model.inventorySummerAle0[1] == 200
-#   elif i == 2:
-#     return model.inventorySummerAle0[2] == 600
-#   else:
-#     return model.inventorySummerAle0[i] == model.summerAle1[i-2] + model.summerAle2[i-2] + model.summerAle3[i-2]
-
-# def balanceSummerAle1(model, i):
-#   if i==1:
-#     return model.inventorySummerAle1[1] == 0
-#   else:
-#     return model.inventorySummerAle1[i] == model.inventorySummerAle0[i-1] - model.sellSummerAle0[i-1]
-
-# def balanceSummerAle2(model, i):
-#   if i==1:
-#     return model.inventorySummerAle2[1] == 0
-#   else:
-#     return model.inventorySummerAle2[i] == model.inventorySummerAle1[i-1] - model.sellSummerAle1[i-1]
-
-# def balanceSummerAle3(model, i):
-#   if i==1:
-#     return model.inventorySummerAle3[1] == 0
-#   else:
-#     return model.inventorySummerAle3[i] == model.inventorySummerAle2[i-1] - model.sellSummerAle2[i-1]
-
-# def balanceExpiredSummerAle(model, i):
-#   if i==1:
-#     return model.expiredSummerAle[1] == 0
-#   else:
-#     return model.expiredSummerAle[i] == model.inventorySummerAle3[i-1] - model.sellSummerAle3[i-1]
-
-# def sellSummerAle0Cap(model, i):
-#   return model.sellSummerAle0[i] <= model.inventorySummerAle0[i]
-# def sellSummerAle1Cap(model, i):
-#   return model.sellSummerAle1[i] <= model.inventorySummerAle1[i]
-# def sellSummerAle2Cap(model, i):
-#   return model.sellSummerAle2[i] <= model.inventorySummerAle2[i]
-# def sellSummerAle3Cap(model, i):
-#   return model.sellSummerAle3[i] <= model.inventorySummerAle3[i]
-
-# def sellSummerAle3Demand(model, i):
-#   return model.sellSummerAle3[i] <= summerAleDemand[i]
-# def sellSummerAle2Demand(model, i):
-#   return model.sellSummerAle2[i] <= summerAleDemand[i] - model.sellSummerAle3[i]
-# def sellSummerAle1Demand(model, i):
-#   return model.sellSummerAle1[i] <= summerAleDemand[i] - model.sellSummerAle3[i] - model.sellSummerAle2[i]
-# def sellSummerAle0Demand(model, i):
-#   return model.sellSummerAle0[i] <= summerAleDemand[i] - model.sellSummerAle3[i] - model.sellSummerAle2[i] - model.sellSummerAle1[i]
-
 ### WinterBrew Inventory
 
 def balanceWinterBrew0(model, i):
@@ -435,7 +360,8 @@ def sellWinterBrew1Demand(model, i):
   return model.sellWinterBrew1[i] <= winterBrewDemand[i] - model.sellWinterBrew3[i] - model.sellWinterBrew2[i]
 model.sellWinterBrew1DemandConstraint = Constraint(model.index, rule=sellWinterBrew1Demand)
 def sellWinterBrew0Demand(model, i):
-  return model.sellWinterBrew0[i] <= winterBrewDemand[i] - model.sellWinterBrew3[i] - model.sellWinterBrew2[i] - model.sellWinterBrew1[i]
+  return model.sellWinterBrew0[i] <= winterBrewDemand[i] - model.sellWinterBrew3[i] - model.sellWinterBrew2[i] \
+    - model.sellWinterBrew1[i]
 model.sellWinterBrew0DemandConstraint = Constraint(model.index, rule=sellWinterBrew0Demand)
 
 ### Octoberfest Inventory
@@ -493,7 +419,8 @@ def sellOctoberfest1Demand(model, i):
   return model.sellOctoberfest1[i] <= octoberfestDemand[i] - model.sellOctoberfest3[i] - model.sellOctoberfest2[i]
 model.sellOctoberfest1DemandConstraint = Constraint(model.index, rule=sellOctoberfest1Demand)
 def sellOctoberfest0Demand(model, i):
-  return model.sellOctoberfest0[i] <= octoberfestDemand[i] - model.sellOctoberfest3[i] - model.sellOctoberfest2[i] - model.sellOctoberfest1[i]
+  return model.sellOctoberfest0[i] <= octoberfestDemand[i] - model.sellOctoberfest3[i] - model.sellOctoberfest2[i] \
+    - model.sellOctoberfest1[i]
 model.sellOctoberfest0DemandConstraint = Constraint(model.index, rule=sellOctoberfest0Demand)
 
 ### Fermenters Usage
@@ -501,203 +428,32 @@ def fermenter1(model, i):
   if i == 1:
     return model.yPaleAle1[1] + model.yStout1[1] + model.yWinterBrew1[i] + model.yOctoberfest1[1] == 0
   elif i==2:
-    return model.yPaleAle1[i] + model.yPaleAle1[i-1] + model.yStout1[i] + model.yStout1[i-1] + model.yWinterBrew1[i] + model.yWinterBrew1[i-1] + model.yOctoberfest1[i] + model.yOctoberfest1[i-1] <= 1
+    return model.yPaleAle1[i] + model.yPaleAle1[i-1] + model.yStout1[i] + model.yStout1[i-1] + model.yWinterBrew1[i] \
+      + model.yWinterBrew1[i-1] + model.yOctoberfest1[i] + model.yOctoberfest1[i-1] <= 1
   else:
-    return model.yPaleAle1[i] + model.yPaleAle1[i-1] + model.yStout1[i] + model.yStout1[i-1] + model.yStout1[i-2] + model.yWinterBrew1[i] + model.yWinterBrew1[i-1] + model.yOctoberfest1[i] + model.yOctoberfest1[i-1] <= 1
+    return model.yPaleAle1[i] + model.yPaleAle1[i-1] + model.yStout1[i] + model.yStout1[i-1] + model.yStout1[i-2] \
+      + model.yWinterBrew1[i] + model.yWinterBrew1[i-1] + model.yOctoberfest1[i] + model.yOctoberfest1[i-1] <= 1
 model.fermenter1Constraint = Constraint(model.index, rule=fermenter1)
 def fermenter2(model, i):
   if i == 1:
     return model.yPaleAle2[1] + model.yStout2[1] + model.yWinterBrew2[i] + model.yOctoberfest2[1] == 0
   elif i==2:
-    return model.yPaleAle2[i] + model.yPaleAle2[i-1] + model.yStout2[i] + model.yStout2[i-1] + model.yWinterBrew2[i] + model.yWinterBrew2[i-1] + model.yOctoberfest2[i] + model.yOctoberfest2[i-1] <= 1
+    return model.yPaleAle2[i] + model.yPaleAle2[i-1] + model.yStout2[i] + model.yStout2[i-1] + model.yWinterBrew2[i] \
+      + model.yWinterBrew2[i-1] + model.yOctoberfest2[i] + model.yOctoberfest2[i-1] <= 1
   else:
-    return model.yPaleAle2[i] + model.yPaleAle2[i-1] + model.yStout2[i] + model.yStout2[i-1] + model.yStout2[i-2] + model.yWinterBrew2[i] + model.yWinterBrew2[i-1] + model.yOctoberfest2[i] + model.yOctoberfest2[i-1] <= 1
+    return model.yPaleAle2[i] + model.yPaleAle2[i-1] + model.yStout2[i] + model.yStout2[i-1] + model.yStout2[i-2] \
+      + model.yWinterBrew2[i] + model.yWinterBrew2[i-1] + model.yOctoberfest2[i] + model.yOctoberfest2[i-1] <= 1
 model.fermenter2Constraint = Constraint(model.index, rule=fermenter2)
 def fermenter3(model, i):
   if i == 1:
     return model.yPaleAle3[1] + model.yStout3[1] + model.yWinterBrew3[1] + model.yOctoberfest3[1] <= 1
   elif i==2:
-    return model.yPaleAle3[i] + model.yPaleAle3[i-1] + model.yStout3[i] + model.yStout3[i-1] + model.yWinterBrew3[i] + model.yWinterBrew3[i-1] + model.yOctoberfest3[i] + model.yOctoberfest3[i-1] <= 1
+    return model.yPaleAle3[i] + model.yPaleAle3[i-1] + model.yStout3[i] + model.yStout3[i-1] + model.yWinterBrew3[i] \
+      + model.yWinterBrew3[i-1] + model.yOctoberfest3[i] + model.yOctoberfest3[i-1] <= 1
   else:
-    return model.yPaleAle3[i] + model.yPaleAle3[i-1] + model.yStout3[i] + model.yStout3[i-1] + model.yStout3[i-2] + model.yWinterBrew3[i] + model.yWinterBrew3[i-1] + model.yOctoberfest3[i] + model.yOctoberfest3[i-1] <= 1
+    return model.yPaleAle3[i] + model.yPaleAle3[i-1] + model.yStout3[i] + model.yStout3[i-1] + model.yStout3[i-2] \
+      + model.yWinterBrew3[i] + model.yWinterBrew3[i-1] + model.yOctoberfest3[i] + model.yOctoberfest3[i-1] <= 1
 model.fermenter3Constraint = Constraint(model.index, rule=fermenter3)
-
-# model.extraSetupPaleAle1 = Var(model.index, domain=Binary)
-# model.extraSetupPaleAle2 = Var(model.index, domain=Binary)
-# model.extraSetupPaleAle3 = Var(model.index, domain=Binary)
-
-# model.extraSetupStout1 = Var(model.index, domain=Binary)
-# model.extraSetupStout2 = Var(model.index, domain=Binary)
-# model.extraSetupStout3 = Var(model.index, domain=Binary)
-
-# model.extraSetupWinterBrew1 = Var(model.index, domain=Binary)
-# model.extraSetupWinterBrew2 = Var(model.index, domain=Binary)
-# model.extraSetupWinterBrew3 = Var(model.index, domain=Binary)
-
-# model.extraSetupOctoberfest1 = Var(model.index, domain=Binary)
-# model.extraSetupOctoberfest2 = Var(model.index, domain=Binary)
-# model.extraSetupOctoberfest3 = Var(model.index, domain=Binary)
-
-### Setup Switch for Pale Ale
-
-# def extraPaleAle1(model, i):
-#   if i==1:
-#     return model.extraSetupPaleAle1[1] == 0
-#   elif i==2:
-#     return model.extraSetupPaleAle1[2] == 0
-#   elif i==3:
-#     return model.extraSetupPaleAle1[3] == 0
-#   else:
-#     return model.extraSetupPaleAle1[i] >= model.yPaleAle1[i] - model.yPaleAle1[i-2] - model.yPaleAle1[i-3]
-# model.extraPaleAle1Constraint = Constraint(model.index, rule=extraPaleAle1)
-
-# def extraPaleAle2(model, i):
-#   if i==1:
-#     return model.extraSetupPaleAle2[1] == 0
-#   elif i==2:
-#     return model.extraSetupPaleAle2[i] >= model.yPaleAle2[i]
-#   elif i==3:
-#     return model.extraSetupPaleAle2[3] >= model.yPaleAle2[i]
-#   else:
-#     return model.extraSetupPaleAle2[i] >= model.yPaleAle2[i] - model.yPaleAle2[i-2] - model.yPaleAle2[i-3]
-# model.extraPaleAle2Constraint = Constraint(model.index, rule=extraPaleAle2)
-
-# def extraPaleAle3(model, i):
-#   if i==1:
-#     return model.extraSetupPaleAle3[1] >= model.yPaleAle3[1]
-#   elif i==2:
-#     return model.extraSetupPaleAle3[i] >= model.yPaleAle3[i]
-#   elif i==3:
-#     return model.extraSetupPaleAle3[3] >= model.yPaleAle3[i] - model.yPaleAle3[i-2]
-#   else:
-#     return model.extraSetupPaleAle3[i] >= model.yPaleAle3[i] - model.yPaleAle3[i-2] - model.yPaleAle3[i-3]
-# model.extraPaleAle3Constraint = Constraint(model.index, rule=extraPaleAle3)
-
-# ### Setup Switch for Stout
-
-# def extraStout1(model, i):
-#   if i==1:
-#     return model.extraSetupStout1[1] == 0
-#   elif i==2:
-#     return model.extraSetupStout1[2] >= model.yStout1[i]
-#   elif i==3:
-#     return model.extraSetupStout1[3] == model.yStout1[i]
-#   elif i==4:
-#     return model.extraSetupStout1[i] >= model.yStout1[i] - model.yStout1[i-3]
-#   else:
-#     return model.extraSetupStout1[i] >= model.yStout1[i] - model.yStout1[i-3] - model.yStout1[i-4]
-# model.extraStout1Constraint = Constraint(model.index, rule=extraStout1)
-
-# def extraStout2(model, i):
-#   if i==1:
-#     return model.extraSetupStout2[1] == 0
-#   elif i==2:
-#     return model.extraSetupStout2[2] >= model.yStout2[i]
-#   elif i==3:
-#     return model.extraSetupStout2[3] == model.yStout2[i]
-#   elif i==4:
-#     return model.extraSetupStout2[i] >= model.yStout2[i] - model.yStout2[i-3]
-#   else:
-#     return model.extraSetupStout2[i] >= model.yStout2[i] - model.yStout2[i-3] - model.yStout2[i-4]
-# model.extraStout2Constraint = Constraint(model.index, rule=extraStout2)
-
-# def extraStout3(model, i):
-#   if i==1:
-#     return model.extraSetupStout3[1] == 0
-#   elif i==2:
-#     return model.extraSetupStout3[2] >= model.yStout3[i]
-#   elif i==3:
-#     return model.extraSetupStout3[3] == model.yStout3[i]
-#   elif i==4:
-#     return model.extraSetupStout3[i] >= model.yStout3[i] - model.yStout3[i-3]
-#   else:
-#     return model.extraSetupStout3[i] >= model.yStout3[i] - model.yStout3[i-3] - model.yStout3[i-4]
-# model.extraStout3Constraint = Constraint(model.index, rule=extraStout3)
-
-# ### Extra Setup for WinterBrew
-
-# def extraWinterBrew1(model, i):
-#   if i==1:
-#     return model.extraSetupWinterBrew1[1] >= model.yWinterBrew1[1]
-#   elif i==2:
-#     return model.extraSetupWinterBrew1[i] >= model.yWinterBrew1[i]
-#   elif i==3:
-#     return model.extraSetupWinterBrew1[3] >= model.yWinterBrew1[i] - model.yWinterBrew1[i-2]
-#   else:
-#     return model.extraSetupWinterBrew1[i] >= model.yWinterBrew1[i] - model.yWinterBrew1[i-2] - model.yWinterBrew1[i-3]
-# model.extraWinterBrew1Constraint = Constraint(model.index, rule=extraWinterBrew1)
-
-# def extraWinterBrew2(model, i):
-#   if i==1:
-#     return model.extraSetupWinterBrew2[1] >= model.yWinterBrew2[1]
-#   elif i==2:
-#     return model.extraSetupWinterBrew2[i] >= model.yWinterBrew2[i]
-#   elif i==3:
-#     return model.extraSetupWinterBrew2[3] >= model.yWinterBrew2[i] - model.yWinterBrew2[i-2]
-#   else:
-#     return model.extraSetupWinterBrew2[i] >= model.yWinterBrew2[i] - model.yWinterBrew2[i-2] - model.yWinterBrew2[i-3]
-# model.extraWinterBrew2Constraint = Constraint(model.index, rule=extraWinterBrew2)
-
-# def extraWinterBrew3(model, i):
-#   if i==1:
-#     return model.extraSetupWinterBrew3[1] >= model.yWinterBrew3[1]
-#   elif i==2:
-#     return model.extraSetupWinterBrew3[i] >= model.yWinterBrew3[i]
-#   elif i==3:
-#     return model.extraSetupWinterBrew3[3] >= model.yWinterBrew3[i] - model.yWinterBrew3[i-2]
-#   else:
-#     return model.extraSetupWinterBrew3[i] >= model.yWinterBrew3[i] - model.yWinterBrew3[i-2] - model.yWinterBrew3[i-3]
-# model.extraWinterBrew3Constraint = Constraint(model.index, rule=extraWinterBrew3)
-
-# ### Extra Setup for Octoberfest
-
-# def extraOctoberfest1(model, i):
-#   if i==1:
-#     return model.extraSetupOctoberfest1[1] >= model.yOctoberfest1[1]
-#   elif i==2:
-#     return model.extraSetupOctoberfest1[i] >= model.yOctoberfest1[i]
-#   elif i==3:
-#     return model.extraSetupOctoberfest1[3] >= model.yOctoberfest1[i] - model.yOctoberfest1[i-2]
-#   else:
-#     return model.extraSetupOctoberfest1[i] >= model.yOctoberfest1[i] - model.yOctoberfest1[i-2] - model.yOctoberfest1[i-3]
-# model.extraOctoberfest1Constraint = Constraint(model.index, rule=extraOctoberfest1)
-
-# def extraOctoberfest2(model, i):
-#   if i==1:
-#     return model.extraSetupOctoberfest2[1] >= model.yOctoberfest2[1]
-#   elif i==2:
-#     return model.extraSetupOctoberfest2[i] >= model.yOctoberfest2[i]
-#   elif i==3:
-#     return model.extraSetupOctoberfest2[3] >= model.yOctoberfest2[i] - model.yOctoberfest2[i-2]
-#   else:
-#     return model.extraSetupOctoberfest2[i] >= model.yOctoberfest2[i] - model.yOctoberfest2[i-2] - model.yOctoberfest2[i-3]
-# model.extraOctoberfest2Constraint = Constraint(model.index, rule=extraOctoberfest2)
-
-# def extraOctoberfest3(model, i):
-#   if i==1:
-#     return model.extraSetupOctoberfest3[1] >= model.yOctoberfest3[1]
-#   elif i==2:
-#     return model.extraSetupOctoberfest3[i] >= model.yOctoberfest3[i]
-#   elif i==3:
-#     return model.extraSetupOctoberfest3[3] >= model.yOctoberfest3[i] - model.yOctoberfest3[i-2]
-#   else:
-#     return model.extraSetupOctoberfest3[i] >= model.yOctoberfest3[i] - model.yOctoberfest3[i-2] - model.yOctoberfest3[i-3]
-# model.extraOctoberfest3Constraint = Constraint(model.index, rule=extraOctoberfest3)
-
-# ### Setup Pale Ale
-# def sameSetupCostPaleAle1(model, i):
-#   return model.setupPaleAle1[i] >= 150 * model.yPaleAle1[i]
-
-# def setupCostPaleAle1(model, i):
-#   if i == 1:
-#     return model.setupPaleAle1[1] == 0
-#   elif i == 2:
-#     return model.setupPaleAle1[2] == 150 * model.yPaleAle1[2]
-#   elif i == 3:
-#     return model.setupPaleAle1[i] >= (1 - model.yPaleAle1[i-2]) * 250
-#   else:
-#     return model.setupPaleAle1[i] >= (1 - model.yPaleAle1[i-2] - model.yPaleAle1[i-3]) * 250
-
 
 def obj_expression(model):
     return 6 * summation(model.sellPaleAle0) + 6 * summation(model.sellPaleAle1) + 6 * summation(model.sellPaleAle2) + 6 * summation(model.sellPaleAle3) \
@@ -720,13 +476,6 @@ def obj_expression(model):
     - 150 * (summation(model.yStout1) + summation(model.yStout2) + summation(model.yStout3)) \
     - 150 * (summation(model.yWinterBrew1) + summation(model.yWinterBrew2) + summation(model.yWinterBrew3)) \
     - 150 * (summation(model.yOctoberfest1) + summation(model.yOctoberfest2) + summation(model.yOctoberfest3)) \
-    # - 100 * (summation(model.extraSetupPaleAle1) + summation(model.extraSetupPaleAle2) + summation(model.extraSetupPaleAle3)) \
-    # - 100 * (summation(model.extraSetupStout1) + summation(model.extraSetupStout2) + summation(model.extraSetupStout3)) \
-    # - 100 * (summation(model.extraSetupWinterBrew1) + summation(model.extraSetupWinterBrew2) + summation(model.extraSetupWinterBrew3)) \
-    # - 100 * (summation(model.extraSetupOctoberfest1) + summation(model.extraSetupOctoberfest2) + summation(model.extraSetupOctoberfest3)) \
-
-
-
 
 model.OBJ = Objective(rule=obj_expression, sense=maximize)
 
